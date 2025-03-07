898d1f75f43e0a742d3488e152d1e35cf0b66105
Juhplux <68930642+Juhplux@users.noreply.github.com>
2025-03-06 19:12:25 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFunction1() {
+    window.location.href = 'level1.html';
+}
+function myFunction2() {
+    window.location.href = 'level2.html';
+}
+function myFunction3() {
+    window.location.href = 'level3.html';
+}
+function myFunction4() {
+    window.location.href = 'level4.html';
+}
